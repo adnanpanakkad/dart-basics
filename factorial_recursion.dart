edf29a26f71563bdbfa36b3533d3e76d91550a5d
Adnan <adnancpanakkad@gmail.com>
2024-03-13 17:15:26 +0530

    parametr concept

--- factorial_recursion.dart
+++ factorial_recursion.dart
@@ -1,13 +1,13 @@
 int factorial(int n) {
   if (n <= 1) {
-    return 1;
+    return n;
   } else {
     return n * factorial(n - 1);
   }
 }
 
 void main() {
-  int num = 3; // You can change this to any non-negative integer
+  int num = 4; // You can change this to any non-negative integer
   int result = factorial(num);
 
   print('The factorial of $num is: $result');
--- new.dart
+++ new.dart
@@ -1,5 +1,5 @@
 void main() {
-  Map students = {"adhil": "10th", "amel": "12th", "arya": "11th"};
-  students["aimen"] = "5th";
-  print(students);
+  String? name;
+ String name1= name ??= 'hadi';
+  print(name1);
 }
